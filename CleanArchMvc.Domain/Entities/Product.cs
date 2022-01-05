8921a81b14504eee191a27494c0acf2221e2d401
Matheus Willian <matheuswill6663@gmail.com>
2022-01-05 14:45:35 -0300

    created ValidateDomain in Product

--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,29 @@ namespace CleanArchMvc.Domain.Entities
         public int Stock { get; private set; }
         public string Image { get; private set; }
 
-        public int CategoryId { get; private set; }
-        public Category Category { get; private set; }
+        private void ValidateDomain(
+            string name,
+            string description,
+            decimal price,
+            int stock,
+            string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "INVALID_NAME. Name is requered");
+            DomainExceptionValidation.When(name.Length < 3, "INVALID_NAME. Too short, minimun 3 character");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "INVALID_DESCRIPTION. Description is requered");
+            DomainExceptionValidation.When(description.Length < 5, "INVALID_DESCRIPTION. Too short, minimun 5 character");
+            DomainExceptionValidation.When(price < 0, "INVALID_PRICE_VALUE");
+            DomainExceptionValidation.When(stock < 0, "INVALID_STOCK_VALUE");
+            DomainExceptionValidation.When(image.Length > 250, "INVALID_IMAGE_NAME. Too long, maximum 250 character");
+
+            Name = name;
+            Description = description; 
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
