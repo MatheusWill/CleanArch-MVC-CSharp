3dcf67eff07ca24facf0afc2cbf2f2c61df99399
Matheus Willian <matheuswill6663@gmail.com>
2022-01-05 14:31:26 -0300

    created Entities Product and Category

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,37 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "INVALID_ID_VALUE");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "INVALID_NAME. Name is requered");
+            DomainExceptionValidation.When(name.Length < 3, "INVALID_NAME. Too short, minimun 3 character");
+
+            Name = name;
+        }
+    }
+}
