2aa9539cd88d4edc591e44b29333a41a18d72c10
Matheus Willian <matheuswill6663@gmail.com>
2022-01-05 14:35:12 -0300

    add method Update

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -24,6 +24,11 @@ namespace CleanArchMvc.Domain.Entities
             ValidateDomain(name);
         }
 
+        public void Update (string name)
+        {
+            ValidateDomain (name);
+        }
+
         public ICollection<Product> Products { get; set; }
 
         private void ValidateDomain(string name)
