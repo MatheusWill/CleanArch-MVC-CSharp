25f9cf08886b55d83e4c282a8d2380541abb54f4
Matheus Willian <matheuswill6663@gmail.com>
2022-01-06 16:03:41 -0300

    create migration and seed

--- CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -12,6 +12,7 @@ namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
             builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
             builder.Property(p => p.Description).HasMaxLength(200).IsRequired();
             builder.Property(p => p.Price).HasPrecision(10, 2);
+            builder.Property(p => p.Image).HasMaxLength(250);
 
             builder.HasOne(e => e.Category).WithMany(e => e.Products).HasForeignKey(e => e.CategoryId);
         }
